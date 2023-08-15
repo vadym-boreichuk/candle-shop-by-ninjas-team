029da47b46b5476a753af5739e09e6380be8d399
Svitlana Trotsenko <trots.svit@gmail.com>
2023-08-15 15:09:24 +0100

    start hero

--- src/components/main/hero/Hero.jsx
+++ src/components/main/hero/Hero.jsx
@@ -1,3 +1,66 @@
+import React from 'react'
+// import { useEffect, useState, useRef } from 'react';
+// import { Link, NavLink } from 'react-router-dom';
+
+import {
+	HeroContainer,
+	HeroContent,
+	HeroImg,
+	HeroSection,
+	HeroText,
+	StyledHeroButton,
+	StyledHeroText,
+	StyledHeroTitle,
+} from './Hero.styled.js'
+
 export const Hero = () => {
-  return <div></div>;
-};
+	return (
+		<HeroSection>
+			<HeroContainer>
+				<HeroContent>
+					<HeroText>
+						<StyledHeroTitle>
+							Великий асортимент воскових виробів
+						</StyledHeroTitle>
+						<StyledHeroText>
+							Підберемо особливий аромат та створимо атмосферу для вашої оселі
+						</StyledHeroText>
+
+						<StyledHeroButton type='button'>
+							<a
+								href='../gallery/Galery.jsx'
+								style={{
+									color: 'white',
+									textDecoration: 'none',
+									display: 'block',
+									width: '100%',
+									textAlign: 'center',
+								}}
+							>
+								Каталог
+							</a>
+						</StyledHeroButton>
+					</HeroText>
+					<HeroImg>
+						<img
+							src={require('../../../assets/images/hero-img.png')}
+							alt='Hero'
+							style={{
+								position: 'absolute',
+								paddingLeft: '15px',
+								paddingTop: '19px',
+							}}
+						/>
+						<img
+							src={require('../../../assets/images/frame.png')}
+							alt='Hero in frame'
+							style={{
+								position: 'absolute',
+							}}
+						></img>
+					</HeroImg>
+				</HeroContent>
+			</HeroContainer>
+		</HeroSection>
+	)
+}
---
+++ src/components/main/hero/Hero.styled.js
@@ -0,0 +1,58 @@
+import { styled } from 'styled-components'
+export const HeroSection = styled.section`
+	background-color: #f5ebe5;
+	padding: 134px 75px 223px 69px;
+`
+
+export const HeroContainer = styled.div`
+	// width: 1287px;
+	// hight: 807px;
+`
+
+export const HeroContent = styled.div`
+	display: flex;
+	gap: 31px;
+`
+
+export const HeroText = styled.div`
+	width: 819px;
+	padding-top: 42px;
+`
+export const StyledHeroTitle = styled.div`
+	font-family: 'Monotype Corsiva';
+	font-weight: 500;
+	font-size: 92px;
+
+	text-decoration: none;
+	color: #000000;
+	margin-bottom: 37px;
+`
+export const StyledHeroText = styled.div`
+	font-family: 'Montserat';
+
+	font-size: 32px;
+	line-height: 1.38;
+	text-decoration: none;
+	color: #050505;
+	font-size: 32px;
+	margin-bottom: 37px;
+`
+export const StyledHeroButton = styled.div`
+	height: 75px;
+	width: 324px;
+	background-color: #600c26;
+	border-radius: 44px;
+	color: #ffffff;
+	border: none;
+	padding: 17.5px 100.5px;
+	font-size: 32px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	cursor: pointer;
+`
+export const HeroImg = styled.section`
+	height: 415px;
+	weight: 540px;
+	// border: 2px solid #600c26;
+`
