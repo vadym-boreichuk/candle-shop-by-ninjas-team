ed38da18d1d56132fdae3e2c0943d3342ceed403
Svitlana Trotsenko <trots.svit@gmail.com>
2023-08-20 02:56:27 +0100

    optimization of buttons
    Fixed the frame of the picture, but still dont have video. Add button Play for player. Button is with hover and focus color option and link to the Galary.

--- src/components/main/hero/Hero.jsx
+++ src/components/main/hero/Hero.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 // import { useEffect, useState, useRef } from 'react';
-// import { Link, NavLink } from 'react-router-dom';
+import heroImg1x from '../../../assets/images/hero-img1x.png'
+import heroImg2x from '../../../assets/images/hero-img2x.png'
+import heroPlay1x from '../../../assets/images/hero-play1x.png'
+import heroPlay2x from '../../../assets/images/hero-play2x.png'
 
+import { StyledButton } from '../../Button/Button.styled.js'
 import {
+	ButtonPlay,
 	HeroContainer,
 	HeroContent,
 	HeroImg,
 	HeroSection,
 	HeroText,
-	StyledHeroButton,
 	StyledHeroText,
 	StyledHeroTitle,
+	StyledLink,
 } from './Hero.styled.js'
 
 export const Hero = () => {
@@ -25,39 +30,53 @@ export const Hero = () => {
 						<StyledHeroText>
 							Підберемо особливий аромат та створимо атмосферу для вашої оселі
 						</StyledHeroText>
-
-						<StyledHeroButton type='button'>
-							<a
-								href='../gallery/Galery.jsx'
+						<StyledLink
+							to='/catalog'
+							style={{
+								display: 'inline-block',
+								width: '324px',
+							}}
+						>
+							<StyledButton
+								lg='true'
+								type='button'
 								style={{
-									color: 'white',
-									textDecoration: 'none',
-									display: 'block',
-									width: '100%',
-									textAlign: 'center',
+									fontFamily: 'Quicksand',
+									fontWeight: '300px',
+									fontSize: '32px',
+									lineHeight: '40px',
 								}}
 							>
 								Каталог
-							</a>
-						</StyledHeroButton>
+							</StyledButton>
+						</StyledLink>
 					</HeroText>
 					<HeroImg>
 						<img
-							src={require('../../../assets/images/hero-img.png')}
-							alt='Hero'
+							srcSet={`${heroImg1x} 1x, ${heroImg2x} 2x`}
+							src={heroImg1x}
+							alt='Опис зображення для всіх версій'
 							style={{
-								position: 'absolute',
-								paddingLeft: '15px',
-								paddingTop: '19px',
+								// position: 'relative',
+								padding: '19px 14px 15px 15px',
 							}}
 						/>
-						<img
-							src={require('../../../assets/images/frame.png')}
-							alt='Hero in frame'
+						<ButtonPlay
+							type='button'
 							style={{
 								position: 'absolute',
+								top: '50%',
+								left: '50%',
+								transform: 'translate(-50%, -50%)',
+								cursor: 'pointer',
 							}}
-						></img>
+						>
+							<img
+								srcSet={`${heroPlay1x} 1x, ${heroPlay2x} 2x`}
+								src={heroPlay1x}
+								alt='Кнопка Плей'
+							/>
+						</ButtonPlay>
 					</HeroImg>
 				</HeroContent>
 			</HeroContainer>
