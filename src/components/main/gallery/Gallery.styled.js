637258af3368d59c76762aab38e9c8355f42c5a3
Svitlana Trotsenko <trots.svit@gmail.com>
2023-08-22 10:19:10 +0100

    start
    main layout

---
+++ src/components/main/gallery/Gallery.styled.js
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components'
+export const GallerySection = styled.section`
+	padding: 126px 74px 0px 75px;
+`
+export const GalleryBlock = styled.section`
+	display: flex;
+`
+
+export const GalleryBlock1 = styled.section`
+	padding-right: 20px;
+`
+export const GalleryBlock2 = styled.section``
